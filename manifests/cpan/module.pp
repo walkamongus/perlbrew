defb66da989c0d3aefcee189a236bed600733025
Chadwick Banning <walkamongus@users.noreply.github.com>
2014-09-02 16:13:33 -0400

    add cpan::module class for install cpan modules

---
+++ manifests/cpan/module.pp
@@ -0,0 +1,66 @@
+# == Class: perlbrew::cpan::module
+#
+# install cpan modules using a Perlbrew-ed Perl.
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*perlbrew_root*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# [*perl_version*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# [*url*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# [*options*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Variables
+#
+# === Examples
+#
+#  class { perlbrew::cpan::module
+#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ]
+#  }
+#
+# === Authors
+#
+# Chadwick Banning <walkamongus@users.noreply.github.com>
+#
+define perlbrew::cpan::module (
+
+  $perlbrew_root = $perlbrew::params::perlbrew_root,
+  $perl_version  = $perlbrew::perl::version,
+  $url           = '',
+  $options       = [
+    '--install',
+  ],
+
+) {
+
+  require perlbrew
+  require perlbrew::perl
+
+  $pkg_name = $url ? {
+    ''      => $title,
+    default => $url,
+  }
+
+  if (is_array($options)) {
+    $opts = join($options, ' ')
+  }
+
+  exec {"install_${pkg_name}":
+    command => "${perlbrew_root}/perls/perl-${perl_version}/bin/cpanm ${opts} ${pkg_name}",
+    unless  => "${perlbrew_root}/perls/perl-${perl_version}/bin/perl -M${title} -e 1",
+    require => Class['perlbrew::perl']
+  }
+
+}
