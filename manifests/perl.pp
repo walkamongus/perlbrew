ce78efb04ac3ee56992b57a3baeab0a9f3277b4a
Warnar Boekkooi <warnar@cerbex.com>
2014-10-09 13:45:50 +0800

    cpamn also needs bash

--- manifests/perl.pp
+++ manifests/perl.pp
@@ -45,14 +45,15 @@ class perlbrew::perl (
   }
 
   exec {"switch_to_perl_${version}":
-    command  => "/bin/bash --login -c \"source /etc/profile; ${perlbrew::perlbrew_root}/bin/perlbrew switch perl-${version}\"",
+    command  => "/bin/bash --login -c \"${perlbrew::perlbrew_root}/bin/perlbrew switch perl-${version}\"",
     provider => shell,
     unless   => "perl -e 'print $^V' | grep v${version}",
     require  => [ Exec["install_perl_${version}"], File[$perlbrew::perlbrew_init_file], ],
   }
 
   exec{"perl_${version}_install_cpan":
-    command => "/usr/bin/curl -L http://cpanmin.us | ${perlbrew::perlbrew_root}/perls/perl-${version}/bin/perl - App::cpanminus",
+    command => "/bin/bash --login -c \"/usr/bin/curl -L http://cpanmin.us | perl - App::cpanminus\"",
+    provider => shell,
     creates => "${perlbrew::perlbrew_root}/perls/perl-${version}/bin/cpanm",
     require => Exec["switch_to_perl_${version}"],
   }
@@ -67,12 +68,14 @@ class perlbrew::perl (
     order   => 02,
     require => Exec["install_perl_${version}"],
   }
+
   concat::fragment {'perlbrew_path':
     target  => $perlbrew::perlbrew_init_file,
     content => "export PERLBREW_PATH=\"${perlbrew::perlbrew_root}/bin:${perlbrew::perlbrew_root}/perls/perl-${version}/bin\"",
     order   => 03,
     require => Exec["install_perl_${version}"],
   }
+
   concat::fragment {'perlbrew_perl':
     target  => $perlbrew::perlbrew_init_file,
     content => "export PERLBREW_PERL=\"perl-${version}\"",
